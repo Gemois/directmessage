6edb509006d87b168a50755fd5a8b76f25cba5b5
Giorgos Moisidis <37257368+Gemois@users.noreply.github.com>
2024-11-01 20:55:42 +0200

    Project creation with spring initializr

---
+++ src/test/java/com/gmoi/directmessage/DirectmessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoi.directmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectmessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
